4a4bf9778f4dc2034cbace279030e0885a9c2ea1
Iker Alonso <126517793+AIkerVSaul09@users.noreply.github.com>
2023-03-18 00:40:14 +0000

    Peliculas Oscars

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -6,21 +6,41 @@ namespace Pelicula
 
     public class Pelicula
     {
-     
         //Campos
+        private string titulo;
+        private short year;
+        private string pais;
+        private string director;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
-     
+        //public Pelicula (string t,short a)
+        //{
+            //this.titulo=t; this.año=a;
+        //}
         //Métodos
-     
         public void Imprime()
         {
-     //       Console.WriteLine($"{titulo} ({año})");
-
+        //Console.WriteLine($"{titulo} ({año})");
         }
 
+        public void setTitulo(string t)
+        {
+            titulo=t;
+        }
+        public string getTitulo()
+        {
+            return titulo;
+        }
 
+        public void setAño(short a)
+        {
+            year=a;
+        }
+        public short getAño()
+        {
+            return year;
+        }
     }
 
     public class Actor
@@ -31,10 +51,11 @@ namespace Pelicula
 
 
         //Métodos 
-        public void Imprime()
-        {
-            Console.WriteLine($"{Nombre} ({Año})");
-        }
+        //public void Imprime()
+        //{
+            //Console.WriteLine($"{Titulo} ({ano})");
+        //}
+        
     }
 
     // Puedes probar tu código en Main() pero lo importante
@@ -45,11 +66,15 @@ namespace Pelicula
 
 
         static void Main(string[] args)
-        {
-           Pelicula p1 = new Pelicula();
-           p1.SetTitulo("La La Land");
-           p1.SetAño(2016);
-           Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+        {        
+            Pelicula p1 = new Pelicula();           
+            p1.setTitulo("Interestelar");
+            p1.setAño(2014);
+            Pelicula p2 = new Pelicula();
+            p2.setTitulo("Top Gun");
+            p2.setAño(2022);
+            Console.WriteLine($"{p1.getTitulo()} - {p1.getAño()}");
+            Console.WriteLine($"{p2.getTitulo()} - {p2.getAño()}"); 
         }
     }
 }
